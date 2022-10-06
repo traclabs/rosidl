0659b2d7b9d8b1789dd4ebfc061738a941715139
Ana C. Huaman Quispe <ana@traclabs.com>
2022-10-06 11:23:35 -0500

    Compiles and works for rosidl_cli and rosidl_adapter packages

--- rosidl_adapter/setup.py
+++ rosidl_adapter/setup.py
@@ -4,9 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-  packages=['rosidl_cli'],
-  package_dir= {'rosidl_cli': 'rosidl_cli'}, # Should we make this a src folder?
+  packages=['rosidl_adapter'],
+  package_dir= {'rosidl_adapter': 'rosidl_adapter'},
   )
   
 setup(**setup_args)
-
